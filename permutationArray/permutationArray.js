fd3e80b912c95da353e3615bc5122440e301d69d
sameera sood <sameerasood@gmail.com>
2023-04-03 12:23:08 +0100

    test passed for example array

--- permutationArray/permutationArray.js
+++ permutationArray/permutationArray.js
@@ -1,4 +1,11 @@
 const buildArray = function (nums) {
-  return nums;
+  let result = [];
+  for (let i = 0; i < nums.length; i++) {
+    result.push(nums[nums[i]]);
+  }
+  return result;
 };
+
 module.exports = buildArray;
+nums = [0, 2, 1];
+buildArray(nums);
--- permutationArray/permutationArray.test.js
+++ permutationArray/permutationArray.test.js
@@ -2,7 +2,27 @@ const buildArray = require("./permutationArray.js");
 
 describe("zero based permutation array", () => {
   it("returns the same array", () => {
-    const result = buildArray([1]);
-    expect(result).toEqual([1]);
+    const result = buildArray([0]);
+    expect(result).toEqual([0]);
+  });
+
+  it("returns the array with two permutations", () => {
+    const result = buildArray([0, 1]);
+    expect(result).toEqual([0, 1]);
+  });
+
+  it("returns the array with logical permutation", () => {
+    const result = buildArray([0, 2, 1]);
+    expect(result).toEqual([0, 1, 2]);
+  });
+
+  it("returns [0,1,2,4,5,3] for input [0,2,1,5,3,4]", () => {
+    const result = buildArray([0, 1, 2, 4, 5, 3]);
+    expect(result).toEqual[(0, 2, 1, 5, 3, 4)];
+  });
+
+  it("returns [5,0,1,2,3,4] for input [4,5,0,1,2,3]", () => {
+    const result = buildArray([5, 0, 1, 2, 3, 4]);
+    expect(result).toEqual([4, 5, 0, 1, 2, 3]);
   });
 });
